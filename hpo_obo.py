9dc32cc59588041f19fb3f21d230ae055c8bf03e
Gangcai Xie <gcxiester@gmail.com>
2017-10-16 16:25:17 -0400

    add hpo id to hpo standard name method

--- hpo_obo.py
+++ hpo_obo.py
@@ -29,3 +29,28 @@ class Obo:
                     else:
                         u2n_dict[umls]=[name]
         return u2n_dict
+
+    def id2name(self):
+        '''
+        get id---HPO official name dictionary
+        return:
+            HPO id to official name dictionary
+        '''
+        u2n_dict={}
+        with open(self.obo_file) as f:
+            name=''
+            i=0
+            for line in f:
+                line=line.rstrip()
+                if re.search("^name:",line):
+                    name=re.sub("^name: ","",line)
+                    i+=1
+                    continue
+                if re.search("^id:",line):
+                    umls=re.sub("id: ","",line)
+                    if umls in u2n_dict:
+                        u2n_dict[umls].append(name)
+                        print("warning:"+umls+" occurred more than once")
+                    else:
+                        u2n_dict[umls]=[name]
+        return u2n_dict
