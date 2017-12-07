4460478026b2050a19d8fb39f9cd6666a7c751e7
gangcai <gcxiester@gmail.com>
2017-12-06 19:23:42 -0500

    add test function for hpo_obo.py

---
+++ test_hpo_obo.py
@@ -0,0 +1,18 @@
+import hpo_obo as ho
+import unittest
+obj=ho.Obo("db/hp.obo")
+
+class TestStringMethods(unittest.TestCase):
+    def test_synonym2name(self):
+        s2name=obj.synonym2name()
+        self.assertEqual(s2name["Multicystic dysplastic kidney"],"Multicystic kidney dysplasia")
+    def test_id2name(self):
+        id2name_dict=obj.id2name()
+        self.assertEqual(id2name_dict["HP:0000003"],"Multicystic kidney dysplasia")
+    def test_umls2name(self):
+        u2name_dict=obj.umls2name()
+        self.assertEqual(u2name_dict["C3714581"][0],"Multicystic kidney dysplasia")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
