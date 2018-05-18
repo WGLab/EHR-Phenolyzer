9c54d6c5105d5e6c14b518684d4caf7c268d619a
gangcai <gcxiester@gmail.com>
2018-05-18 18:39:27 -0400

    fix negex extraction problems

--- lib/parse_metamap_output.py
+++ lib/parse_metamap_output.py
@@ -7,11 +7,22 @@ class OutputParser:
     #get the umls ids from metamap output file
     def extract_umls_id(self):
         ids={}
+	neg_ids={}
         for line in open(self.input):
             if re.search("^Processing",line):
                 continue
             if re.search("^Meta Mapping",line):
                 continue
+	    items=line.split(":")[0].split()
+	    negex=items[-2]
             id=line.split(":")[0].split()[-1]
             ids[id]=0
-        return ids.keys()
+	    if len(items)==3 and negex=='N':
+		    neg_ids[id]=0
+	ids_used=[]
+	for id_ in ids:
+		if not id_ in neg_ids:
+			ids_used.append(id_)
+		else:
+			print("Notes: Negative UMLS ID found:"+id_+", which will be ignored")
+        return ids_used
