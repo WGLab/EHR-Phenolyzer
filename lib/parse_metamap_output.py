2a8f79f8f9e9ef01d650f0b53bc8690a7e2e13df
Li Yin <liyin@scripps.edu>
2018-12-19 12:05:03 -0800

    Styled using black

--- lib/parse_metamap_output.py
+++ lib/parse_metamap_output.py
@@ -1,28 +1,32 @@
 import re
-class OutputParser:
-    #input: the metamap output text file
-    def __init__(self,input):
-        self.input=input
 
-    #get the umls ids from metamap output file
+
+class OutputParser:
+    # input: the metamap output text file
+    def __init__(self, input):
+        self.input = input
+
+    # get the umls ids from metamap output file
     def extract_umls_id(self):
-        ids={}
-        neg_ids={}
+        ids = {}
+        neg_ids = {}
         for line in open(self.input):
-            if re.search("^Processing",line):
+            if re.search("^Processing", line):
                 continue
-            if re.search("^Meta Mapping",line):
+            if re.search("^Meta Mapping", line):
                 continue
-            items=line.split(":")[0].split()
-            negex=items[-2]
-            id=line.split(":")[0].split()[-1]
-            ids[id]=0
-            if len(items)==3 and negex=='N':
-                neg_ids[id]=0
-        ids_used=[]
+            items = line.split(":")[0].split()
+            negex = items[-2]
+            id = line.split(":")[0].split()[-1]
+            ids[id] = 0
+            if len(items) == 3 and negex == "N":
+                neg_ids[id] = 0
+        ids_used = []
         for id_ in ids:
             if not id_ in neg_ids:
                 ids_used.append(id_)
             else:
-                print("Notes: Negative UMLS ID found:"+id_+", which will be ignored")
+                print(
+                    "Notes: Negative UMLS ID found:" + id_ + ", which will be ignored"
+                )
         return ids_used
--- lib/pymetamap.py
+++ lib/pymetamap.py
@@ -1,71 +1,75 @@
 import os
 from lib.parse_metamap_output import OutputParser
-#import obo as ob # obo from third party, TODO: write my own obo file parser
+
+# import obo as ob # obo from third party, TODO: write my own obo file parser
 from lib.hpo_obo import Obo
+
 
 def run_command(command_line):
     return os.popen(command_line).read()
 
 
-#Remove abnormal characters in the input file, otherwise will lead to system error for metamap
-#TODO: consider more cases
-def format_input(notes_file,outdir):
+# Remove abnormal characters in the input file, otherwise will lead to system error for metamap
+# TODO: consider more cases
+def format_input(notes_file, outdir):
     def removeNonAscii(s):
-        return "".join(i for i in s if ord(i)<128)
+        return "".join(i for i in s if ord(i) < 128)
 
-    input_tmp_name=outdir+"/"+filename.split("/")[-1]+".tmp"
-    input_tmp=open(input_tmp_name,"w")
-    input_str=""
+    input_tmp_name = outdir + "/" + filename.split("/")[-1] + ".tmp"
+    input_tmp = open(input_tmp_name, "w")
+    input_str = ""
     for line in open(filename):
-        input_str=input_str+line
+        input_str = input_str + line
 
-    input_str_noascii=removeNonAscii(input_str)
+    input_str_noascii = removeNonAscii(input_str)
     input_tmp.write(input_str_noascii)
     input_tmp.close()
     return 0
 
 
-def get_hpo_names(umlsid_file,outfile_name,obo_file,outdir="./"):
-    names={}
-    op=Obo(obo_file)
-    name_dict=op.umls2name()
+def get_hpo_names(umlsid_file, outfile_name, obo_file, outdir="./"):
+    names = {}
+    op = Obo(obo_file)
+    name_dict = op.umls2name()
     with open(umlsid_file) as f:
         for line in f:
-            uid=line.rstrip()
+            uid = line.rstrip()
             if uid in name_dict:
-                name_arr=name_dict[uid]
+                name_arr = name_dict[uid]
             else:
-                print("Warning:"+uid+" not found in HPO database")
+                print("Warning:" + uid + " not found in HPO database")
                 continue
             for name in name_arr:
-                names[name]=0
-    outfile=open(outdir+"/"+outfile_name,"w")
+                names[name] = 0
+    outfile = open(outdir + "/" + outfile_name, "w")
     for name in names.keys():
-        outfile.write(name+"\n")
+        outfile.write(name + "\n")
     outfile.close()
     return 0
 
 
-def run_metamap(notes_file,prefix,obo_file,outdir="./"):
+def run_metamap(notes_file, prefix, obo_file, outdir="./"):
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    commands="metamap -I -p -J -K -8 --conj cgab,genf,lbpr,lbtr,patf,dsyn,fndg -R 'HPO' {0} {1}/{2}.metamap.o".format(notes_file,outdir,prefix)
-    #start the server
+    commands = "metamap -I -p -J -K -8 --conj cgab,genf,lbpr,lbtr,patf,dsyn,fndg -R 'HPO' {0} {1}/{2}.metamap.o".format(
+        notes_file, outdir, prefix
+    )
+    # start the server
     print(run_command("skrmedpostctl start"))
-    #run metamap
+    # run metamap
     print(run_command(commands))
-    #stop the server
-    #try:
+    # stop the server
+    # try:
     #    print(run_command("skrmedpostctl stop"))
-    #except:
+    # except:
     #    print("warning: stop skrmedpostctl failed")
-    #extract umls ids
-    o_filename=outdir+"/"+prefix+".metamap.o"
-    op=OutputParser(o_filename)
-    ids=op.extract_umls_id()
-    umlsid_file=outdir+"/"+prefix+".umlsids.txt"
-    outfile=open(umlsid_file,"w")
+    # extract umls ids
+    o_filename = outdir + "/" + prefix + ".metamap.o"
+    op = OutputParser(o_filename)
+    ids = op.extract_umls_id()
+    umlsid_file = outdir + "/" + prefix + ".umlsids.txt"
+    outfile = open(umlsid_file, "w")
     outfile.write("\n".join(ids))
     outfile.close()
-    hpo_file=prefix+".hpo.txt"
-    get_hpo_names(umlsid_file,hpo_file,obo_file,outdir)
+    hpo_file = prefix + ".hpo.txt"
+    get_hpo_names(umlsid_file, hpo_file, obo_file, outdir)
